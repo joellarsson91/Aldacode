c15a23d605acaa2d6d3d32494db01254f3e738ee
joellarsson91 <49025943+joellarsson91@users.noreply.github.com>
2019-03-28 12:04:14 +0100

    Add files via upload

---
+++ aldaqu/src/Test.java
@@ -0,0 +1,32 @@
+//package alda.linear;
+//
+//public class Test {
+//	
+//	public static void main(String[] args) {
+//		
+//		MyQueue<String> que = new MyQueue<>(20);
+//		que.add("1");
+//		que.add("2");
+//		que.add("3");
+//		que.add("Blala");
+//		System.out.println("Första objektet i listan är: " + que.peek() + 
+//				" och listan är "+ que.size() + " noder stor." + 
+//				" Är kölistan tom? " + que.isEmpty() +
+//				" Är kölistan full? " + que.isFull() +
+//				"Hur stor är kapaciteten på kölistan? " + que.totalCapacity() +
+//				"Hur många platser finns kvar i kölistan? " + que.currentCapacity());
+//		
+//		
+//		que.clear();
+//		System.out.println("Första objektet i listan är: " + que.peek() + 
+//				" och listan är "+ que.size() + " noder stor." + 
+//				" Är kölistan tom? " + que.isEmpty() +
+//				" Är kölistan full? " + que.isFull() +
+//				"Hur stor är kapaciteten på kölistan? " + que.totalCapacity() +
+//				"Hur många platser finns kvar i kölistan? " + que.currentCapacity());
+//		
+//		
+//		
+//	}
+//
+//}
